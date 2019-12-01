375537077d8f6587a41ac7cf698e839ef3cddc00
Kedar Parsewar <42874351+kedar1023@users.noreply.github.com>
2019-12-01 23:37:37 +0530

    Add files via upload

---
+++ complex/include/config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', '4hWspuooqkdYC6j9QpQVGulzz');
+define('CONSUMER_SECRET', 'ptUK4M0rxigSwJ7OrzazZV6PM5jvsraMITH5VDgzkqCPTUl6Sg');
+define('OAUTH_CALLBACK', 'http://localhost/complex/process.php');
+?>
---
+++ complex/twitter/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
+define('DATUMBOX_API_KEY', '2bb4d687b20d25f2eeaef5febabca111');
+
+// Configure your OAuth settings from your application at https://dev.twitter.com/apps
+
+define('TWITTER_CONSUMER_KEY', 'tNLnfJxI5SknlenWbhWHJhbGd');
+define('TWITTER_CONSUMER_SECRET', 'SOYU0Oea5YLVbd5muaDTy6QoXbU3HhXbKmECQcSL9AohaXBCSj');
+
+
+// Configure authentication credentials.
+// you can generate your own access key from the link above
+
+define('TWITTER_ACCESS_KEY', '985843484379627521-dV2wszOhqa7xKoS9erw06GeHW6lXI3T');
+define('TWITTER_ACCESS_SECRET', 'sbjrUTgOecKQIn1QI7oOXSkPDdmHwuh0hkrTTGDJBETuR'); 
